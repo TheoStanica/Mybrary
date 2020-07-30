09bc3d195195aae93fd00a652b8e0b351ff21bdc
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2020-07-30 16:28:03 +0300

    Initial Author routes

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', "ejs");
 app.set("views", __dirname + "/views");
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({limit: "10mb", extended:false }));
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -26,7 +29,7 @@ db.once('open', () => console.log('connected to db'));
 
 
 app.use('/', indexRouter);
-
+app.use('/authors', authorRouter);
 
 
 
